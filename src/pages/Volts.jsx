6c57f6f0595e34b949c497fb677096c427c3f8b6
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-09-29 01:46:01 +0900

    sdafs

--- src/pages/Volts.jsx
+++ src/pages/Volts.jsx
@@ -75,7 +75,7 @@ function Volts() {
                     </div>
                     <div className="cake-earned">CAKE EARNED</div>
                     <div className="claim">
-                      <div className="claimable-amount zero">98765.43</div>
+                      <div className="claimable-amount zero">0.00000</div>
                       <button className="claim-btn disabled">Harvest</button>
                     </div>
                     <div className="cake-earned">YIELD BOOSTER</div>
@@ -87,7 +87,12 @@ function Volts() {
                     <button className="connect-btn">Connect Wallet</button>
                   </div>
                   <div className="card-bottom"></div>
-                  <div className="card-goto-explore-nochildren">Details</div>
+                  <div className="card-goto-explore-nochildren">
+                    <div className="caption">
+                      Details
+                      <img src="/images/downminarrow.svg" alt="downarrow"></img>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="col-sm-6 col-lg-4 column">
@@ -155,7 +160,342 @@ function Volts() {
                   </div>
                   <div className="card-bottom"></div>
                   <div className="card-goto-explore">
-                    Hide
+                    <div className="caption">
+                      Hide
+                      <img src="/images/upminarrow.svg" alt="uparrow"></img>
+                    </div>
+                    <div className="linquidity">
+                      <div>Total Linquidify:</div>
+                      <div>$282,660</div>
+                    </div>
+                    <div>
+                      Get MONI-BNB
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                    <div>
+                      View Contract
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                    <div>
+                      See Pair Info
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-sm-6 col-lg-4 column">
+                <div className="voltcard">
+                  <div className="card-top">
+                    <div className="top-letf">
+                      <img
+                        className="pair-icon-A"
+                        src="/images/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png"
+                        alt="iconA"
+                      />
+                      <img
+                        className="pair-icon-B"
+                        src="/images/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png"
+                        alt="iconA"
+                      />
+                    </div>
+                    <div className="top-right">
+                      <h2 className="card-title">CAKE-BNB</h2>
+                      <div className="badges">
+                        <div className="core-badge">
+                          <div className="badge-content core-badge-content">
+                            <img
+                              src="/images/checkmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Core
+                          </div>
+                        </div>
+                        <div className="boosted-badge">
+                          <div className="badge-content boosted-badge-content">
+                            <img
+                              src="/images/rocketmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Boosted
+                          </div>
+                        </div>
+                        <div className="multiplex-badge">
+                          <div className="badge-content multiplex-badge-content">
+                            40X
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    <div className="apr">
+                      <div className="apr-left">APR:</div>
+                      <div className="apr-right">98765423</div>
+                    </div>
+                    <div className="earn">
+                      <div className="earn-left">EARN:</div>
+                      <div className="earn-right">CAKE+Fees</div>
+                    </div>
+                    <div className="cake-earned">CAKE EARNED</div>
+                    <div className="claim">
+                      <div className="claimable-amount zero">0.00000</div>
+                      <button className="claim-btn disabled">Harvest</button>
+                    </div>
+                    <div className="cake-earned">YIELD BOOSTER</div>
+                    <div className="up-to-X">Up to 2x</div>
+                    <div className="textSubtle">
+                      Connect wallet to activate yield booster
+                    </div>
+                    <div className="cake-earned">CAKE-BNB LP STAKED</div>
+                    <button className="connect-btn">Connect Wallet</button>
+                  </div>
+                  <div className="card-bottom"></div>
+                  <div className="card-goto-explore-nochildren">
+                    <div className="caption">
+                      Details
+                      <img src="/images/downminarrow.svg" alt="downarrow"></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-lg-4 column">
+                <div className="voltcard">
+                  <div className="card-top">
+                    <div className="top-letf">
+                      <img
+                        className="pair-icon-A"
+                        src="/images/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png"
+                        alt="iconA"
+                      />
+                      <img
+                        className="pair-icon-B"
+                        src="/images/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png"
+                        alt="iconA"
+                      />
+                    </div>
+                    <div className="top-right">
+                      <h2 className="card-title">CAKE-BNB</h2>
+                      <div className="badges">
+                        <div className="stable-badge">
+                          <div className="badge-content stable-badge-content">
+                            Stable LP
+                          </div>
+                        </div>
+                        <div className="core-badge">
+                          <div className="badge-content core-badge-content">
+                            <img
+                              src="/images/checkmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Core
+                          </div>
+                        </div>
+                        <div className="multiplex-badge">
+                          <div className="badge-content multiplex-badge-content">
+                            40X
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    <div className="apr">
+                      <div className="apr-left">APR:</div>
+                      <div className="apr-right">98765423</div>
+                    </div>
+                    <div className="earn">
+                      <div className="earn-left">EARN:</div>
+                      <div className="earn-right">CAKE+Fees</div>
+                    </div>
+                    <div className="cake-earned">CAKE EARNED</div>
+                    <div className="claim">
+                      <div className="claimable-amount">98765.43</div>
+                      <button className="claim-btn">Harvest</button>
+                    </div>
+                    <div className="cake-earned">YIELD BOOSTER</div>
+                    <div className="up-to-X">Up to 2x</div>
+                    <div className="textSubtle">
+                      Connect wallet to activate yield booster
+                    </div>
+                    <div className="cake-earned">CAKE-BNB LP STAKED</div>
+                    <button className="connect-btn">Connect Wallet</button>
+                  </div>
+                  <div className="card-bottom"></div>
+                  <div className="card-goto-explore">
+                    <div className="caption">
+                      Hide
+                      <img src="/images/upminarrow.svg" alt="uparrow"></img>
+                    </div>
+                    <div className="linquidity">
+                      <div>Total Linquidify:</div>
+                      <div>$282,660</div>
+                    </div>
+                    <div>
+                      Get MONI-BNB
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                    <div>
+                      View Contract
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                    <div>
+                      See Pair Info
+                      <img src="/images/toblockscan.svg" alt="toblockscan" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-sm-6 col-lg-4 column">
+                <div className="voltcard">
+                  <div className="card-top">
+                    <div className="top-letf">
+                      <img
+                        className="pair-icon-A"
+                        src="/images/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png"
+                        alt="iconA"
+                      />
+                      <img
+                        className="pair-icon-B"
+                        src="/images/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png"
+                        alt="iconA"
+                      />
+                    </div>
+                    <div className="top-right">
+                      <h2 className="card-title">CAKE-BNB</h2>
+                      <div className="badges">
+                        <div className="core-badge">
+                          <div className="badge-content core-badge-content">
+                            <img
+                              src="/images/checkmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Core
+                          </div>
+                        </div>
+                        <div className="boosted-badge">
+                          <div className="badge-content boosted-badge-content">
+                            <img
+                              src="/images/rocketmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Boosted
+                          </div>
+                        </div>
+                        <div className="multiplex-badge">
+                          <div className="badge-content multiplex-badge-content">
+                            40X
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    <div className="apr">
+                      <div className="apr-left">APR:</div>
+                      <div className="apr-right">98765423</div>
+                    </div>
+                    <div className="earn">
+                      <div className="earn-left">EARN:</div>
+                      <div className="earn-right">CAKE+Fees</div>
+                    </div>
+                    <div className="cake-earned">CAKE EARNED</div>
+                    <div className="claim">
+                      <div className="claimable-amount zero">0.00000</div>
+                      <button className="claim-btn disabled">Harvest</button>
+                    </div>
+                    <div className="cake-earned">YIELD BOOSTER</div>
+                    <div className="up-to-X">Up to 2x</div>
+                    <div className="textSubtle">
+                      Connect wallet to activate yield booster
+                    </div>
+                    <div className="cake-earned">CAKE-BNB LP STAKED</div>
+                    <button className="connect-btn">Connect Wallet</button>
+                  </div>
+                  <div className="card-bottom"></div>
+                  <div className="card-goto-explore-nochildren">
+                    <div className="caption">
+                      Details
+                      <img src="/images/downminarrow.svg" alt="downarrow"></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-lg-4 column">
+                <div className="voltcard">
+                  <div className="card-top">
+                    <div className="top-letf">
+                      <img
+                        className="pair-icon-A"
+                        src="/images/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png"
+                        alt="iconA"
+                      />
+                      <img
+                        className="pair-icon-B"
+                        src="/images/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png"
+                        alt="iconA"
+                      />
+                    </div>
+                    <div className="top-right">
+                      <h2 className="card-title">CAKE-BNB</h2>
+                      <div className="badges">
+                        <div className="stable-badge">
+                          <div className="badge-content stable-badge-content">
+                            Stable LP
+                          </div>
+                        </div>
+                        <div className="core-badge">
+                          <div className="badge-content core-badge-content">
+                            <img
+                              src="/images/checkmark.svg"
+                              className="core-svg"
+                              alt="core"
+                            ></img>
+                            Core
+                          </div>
+                        </div>
+                        <div className="multiplex-badge">
+                          <div className="badge-content multiplex-badge-content">
+                            40X
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    <div className="apr">
+                      <div className="apr-left">APR:</div>
+                      <div className="apr-right">98765423</div>
+                    </div>
+                    <div className="earn">
+                      <div className="earn-left">EARN:</div>
+                      <div className="earn-right">CAKE+Fees</div>
+                    </div>
+                    <div className="cake-earned">CAKE EARNED</div>
+                    <div className="claim">
+                      <div className="claimable-amount">98765.43</div>
+                      <button className="claim-btn">Harvest</button>
+                    </div>
+                    <div className="cake-earned">YIELD BOOSTER</div>
+                    <div className="up-to-X">Up to 2x</div>
+                    <div className="textSubtle">
+                      Connect wallet to activate yield booster
+                    </div>
+                    <div className="cake-earned">CAKE-BNB LP STAKED</div>
+                    <button className="connect-btn">Connect Wallet</button>
+                  </div>
+                  <div className="card-bottom"></div>
+                  <div className="card-goto-explore">
+                    <div className="caption">
+                      Hide
+                      <img src="/images/upminarrow.svg" alt="uparrow"></img>
+                    </div>
                     <div className="linquidity">
                       <div>Total Linquidify:</div>
                       <div>$282,660</div>
